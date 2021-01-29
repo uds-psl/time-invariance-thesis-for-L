d631f8dace7614e2f52c11f971b49536aa22f2db
Yannick Forster <forster@ps.uni-saarland.de>
2021-01-29 14:51:55 +0100

    Invariance Thesis for L w.r.t. Termination, Direction TM to L

--- theories/L/Functions/UnboundIteration.v
+++ theories/L/Functions/UnboundIteration.v
@@ -9,26 +9,27 @@ Section uiter.
   Variable f : X -> X + Y.
 
   Variable fT : timeComplexity (X -> X + Y).
-  Context `{computableTime' f fT}.
 
   Import HOAS_Notations.
-  Definition uiter := Eval cbn -[enc] in rho [L_HOAS (λ uiter x, !!(extT f) x (λ x' _ , uiter x') (λ y _ , y) !!I)].
+  Definition uiter := Eval cbn -[enc] in rho [L_HOAS (λ uiter f x, f x (λ x' _ , uiter f x') (λ y _ , y) !!I)].
   
   Lemma uiter_proc : proc uiter.
   Proof. unfold uiter. Lproc. Qed.
   Hint Resolve uiter_proc : LProc.
 
+  Context `{computableTime' f fT}.
+
   Fixpoint uiterTime n x :=
     match n with
       0 => 0
-    | S n' => fst (fT x tt) + 9 + match f x with
+    | S n' => fst (fT x tt) + 11 + match f x with
                                        inl x' => uiterTime n' x'
                                      | _ => 0
                                      end
     end. 
 
   Lemma uiter_sound n x y:
-    loopSum n f x = Some y -> evalLe (uiterTime n x) (app uiter (enc x)) (enc y).
+    loopSum n f x = Some y -> evalLe (uiterTime n x) (app (app uiter (extT f)) (enc x)) (enc y).
   Proof.
     unfold uiter. Intern.recRem P.
     induction n in x|-*;intros Heq. now easy.
@@ -37,18 +38,43 @@ Section uiter.
     2,3:reflexivity.
     2:{
       destruct (f x) eqn:eqfx.
-      -Intern.recStepNew P. Lsimpl. rewrite eqfx. Lsimpl. eapply IHn. eauto.
+      - Intern.recStepNew P. Lsimpl. rewrite eqfx. Lsimpl. eapply IHn. eauto.
       -inv Heq. Intern.recStepNew P. Lsimpl. rewrite eqfx. Lsimpl. Lreflexivity.
     }
     destruct (f x).
     all:solverec.
   Qed.
 
+  Lemma uiter_complete x t :
+    eval (app (app uiter (extT f)) (enc x)) t -> exists n y, loopSum n f x = Some y. 
+  Proof.
+    unfold uiter. Intern.recRem P.
+    intros [k [Hk Ht]] % eval_evalIn.
+    revert x t Ht Hk. pattern k.
+    eapply complete_induction. clear k. intros k IH x t Ht Hk.
+    assert (app (extT f) (enc x) >(<= fst (fT x tt) + 0) enc (f x)). now Lsimpl. rewrite <- plus_n_O in H4.
+    destruct H4 as (? & ? & ?).
+    destruct (f x) as [x' | ] eqn:E.
+    - assert (exists i, 0 < i <= 3 + (fst (fT x tt) + 7) /\ app (app P (extT f)) (enc x) >(i) app (app P (extT f)) (enc x')).
+      eexists. split. 2:{
+      eapply pow_trans.
+      eapply pow_step_congL. eapply rho_correctPow. Lproc. Lproc. reflexivity.
+      unfold rP. Lsimpl.
+      eapply pow_trans. eapply pow_step_congL. eapply pow_step_congL. eapply pow_step_congL. exact H5.
+      all:try reflexivity.
+      eapply pow_trans with (i := 4). Lsimpl. replace (enc (@inl _ Y x')) with (lam (lam (app #1 (enc x')))) by reflexivity.
+      Lsimpl. reflexivity.
+      } cbn. lia. destruct H6 as (i & ? & ?).
+      eapply  pow_trans_lam' in H7 as (H7 & H8). 3: exact Hk. 2:Lproc.
+      eapply IH in H8 as [n H8]. 2: lia.
+      exists (S n). cbn. now rewrite E. eauto.
+    - exists 1. cbn. rewrite E. eauto.
+  Qed.
   
   Lemma uiter_total_instanceTime {Z} `{registered Z} (f':  Z -> Y) (preprocess : Z -> X) preprocessT (fuel : Z -> nat)
     `{computableTime' preprocess preprocessT} :
     (forall x, loopSum (fuel x) f (preprocess x) = Some (f' x)) ->
-    computesTime (TyArr _ _) f' (convert (λ x, !!uiter (!!(extT preprocess) x))) (fun z _ => (1 + fst (preprocessT z tt) + uiterTime (fuel z) (preprocess z),tt)).
+    computesTime (TyArr _ _) f' (convert (λ x, !!uiter !!(extT f) (!!(extT preprocess) x))) (fun z _ => (1 + fst (preprocessT z tt) + uiterTime (fuel z) (preprocess z),tt)).
   Proof.
     cbn [convert TH "-"].
     intros total.
@@ -58,7 +84,7 @@ Section uiter.
     intros ? ->.
     eexists. split. 2:reflexivity.
     eapply le_evalLe_proper. 2-3:reflexivity.
-    2:{ eapply evalLe_trans with (t := (L.app uiter (enc (preprocess z)))).
+    2:{ eapply evalLe_trans with (t := (L.app (L.app uiter (extT f)) (enc (preprocess z)))).
         -now Lsimpl.
         -eapply uiter_sound. apply total. }
     cbn.
@@ -74,7 +100,7 @@ Section uiter.
               | inr y => forall n', n <= n' -> fst (fT x tt) <= boundL + boundR n'
               end) ->
     P 0 x0 -> 
-    uiterTime n0 x0 <= n0 * (boundL + 9) + boundR n0.
+    uiterTime n0 x0 <= n0 * (boundL + 11) + boundR n0.
   Proof.
     intros H'.
     pose (n0':=n0). assert (Hleq : n0<=n0') by (cbn;lia).
@@ -95,8 +121,8 @@ Section uiter.
 
   Lemma uiterTime_bound_recRel2 (P: nat -> _ -> Prop) (iterT : X -> nat):
     (forall i x, P i x -> match f x with
-                    | inl x' => P (S i) x' /\ iterT x' + 9 + fst (fT x tt) <= iterT x
-                    | inr y => fst (fT x tt) + 9 <= iterT x
+                    | inl x' => P (S i) x' /\ iterT x' + 11 + fst (fT x tt) <= iterT x
+                    | inr y => fst (fT x tt) + 11 <= iterT x
                     end) ->
     forall n x,
       P 0 x -> 
@@ -118,8 +144,8 @@ Section uiter.
 
   Lemma uiterTime_bound_recRel (P: nat -> _ -> Prop) (iterT : nat -> X -> nat) n:
     (forall i x, P i x -> match f x with
-                    | inl x' => P (S i) x' /\ iterT (n-i-1) x' + 9 + fst (fT x tt) <= iterT (n-i) x
-                    | inr y => fst (fT x tt) + 9 <= iterT (n-i) x
+                    | inl x' => P (S i) x' /\ iterT (n-i-1) x' + 11 + fst (fT x tt) <= iterT (n-i) x
+                    | inr y => fst (fT x tt) + 11 <= iterT (n-i) x
                     end) ->
     forall x k,
       k <= n ->
@@ -152,7 +178,7 @@ Section uiter.
                 | inl x' =>  R x x'
                 | _ => True
                 end) ->
-    uiterTime k0 x0 <= k0 * (t__step + 9) + t__end.
+    uiterTime k0 x0 <= k0 * (t__step + 11) + t__end.
   Proof.
     intros H'.
     rewrite uiterTime_bound_onlyByN
@@ -174,6 +200,3 @@ Section uiter.
 End uiter.
 
 Hint Resolve uiter_proc : LProc.
-Arguments uiter _ _ _ _ _ _ _ : clear implicits.
-Arguments uiter {X Y H H0} f {fT H1}.
-
---
+++ theories/L/TM/TimeInvarianceTermination.v
@@ -0,0 +1,130 @@
+(* From Undecidability.Shared.Libs.PSL Require Import FinTypes Vectors.
+From Undecidability.L Require Import TM.TMinL.TMinL_extract.
+From Undecidability.TM Require Import Compiler_facts Compiler_spec. *)
+From Undecidability.L.Datatypes Require Import LNat Lists LProd LFinType LVector.
+From Undecidability.L Require Import Functions.FinTypeLookup Functions.EqBool.
+From Undecidability Require Import TMEncoding.
+
+Require Import Undecidability.L.TM.TMinL.TMinL_extract.
+Require Import Undecidability.L.Functions.UnboundIteration.
+From Undecidability.TM Require Import TM TM_alt TM_facts.
+Require Import Undecidability.L.Util.L_facts.
+From Undecidability.L.Datatypes Require Import LNat.
+
+Import L_Notations.
+Import HOAS_Notations.
+
+Notation PAIR := (lam (lam (lam (0 2 1)))).
+
+Section TimeInvarianceNF.
+
+Variable Σ : finType.
+
+Let reg_sig := @registered_finType Σ.
+Existing Instance reg_sig.
+
+Variable n_tps : nat.
+
+Definition s_sim : term := Eval cbn -[enc] in [L_HOAS (λ M_q0 tps, ha M_q0 (λ M q0, !!uiter M (!!PAIR q0 tps)))]. 
+
+Variable M : TM Σ n_tps.
+Variables (q q' : state M) (t t' : tapes Σ n_tps).
+
+Let reg_state := @registered_finType (state M).
+Existing Instance reg_state.
+
+Definition step_fun := (fun cfg : mconfig Σ (state M) n_tps =>
+                          if haltConf cfg then inr (ctapes cfg) else inl (TM_facts.step cfg)).
+
+Notation STEPTIME := (haltTime M + n_tps* 130+ transTime M + 185).
+
+Instance step_fun_comp : computableTime' step_fun (fun _ _ => (STEPTIME,tt)).
+Proof.
+  extract. solverec.
+Qed.
+
+Lemma TimeInvarianceNF_forward i :
+    loopSumM (M := M) (mk_mconfig q t) i = Some (mk_mconfig q' t') ->
+    evalLe (9 + i * (STEPTIME + 11) ) (s_sim ((PAIR (ext step_fun)) (enc q)) (enc t)) (enc t').
+Proof.
+  intros H.
+  assert (Hi : loopSum i step_fun (mk_mconfig q t) = Some t').
+  { clear - H.  revert H. generalize (mk_mconfig q t). intros cfg H.
+    induction i in H, cfg,t'|-*; cbn in *.
+    - congruence.
+    - unfold step_fun. destruct haltConf. inv H. reflexivity.
+      eapply IHi. eapply H.
+  }
+  eapply evalLe_trans. unfold s_sim. Lsimpl. reflexivity.
+  unshelve eapply uiter_sound in Hi. exact _. exact _. 2: eapply step_fun_comp.
+  unfold s_sim. eapply evalIn_mono. econstructor. 2:Lproc. destruct Hi as [Hi ?].
+  unfold enc at 1 in Hi. cbn in Hi. unfold enc at 1 in Hi. cbn in Hi.
+  Lsimpl. reflexivity. rewrite <- plus_n_O.
+  clear. fold (transTime M).
+  change (  uiterTime step_fun
+    (fun (_ : mconfig Σ (state M) n_tps) (_ : unit) =>
+     (haltTime M + n_tps * 130 +
+      transTime M + 185, tt)) i (mk_mconfig q t) <= i * (STEPTIME + 11)).
+  generalize STEPTIME as C. intros C. generalize (mk_mconfig q t). intros cfg. induction i in cfg |- *.
+  - cbn. lia. 
+  - cbn. destruct _. rewrite IHi. lia. lia.
+Qed.
+
+Lemma TimeInvarianceNF_backward v :
+  eval (s_sim ((PAIR (ext step_fun)) (enc q)) (enc t)) v -> exists q' t' i, loopSumM (M := M) (mk_mconfig q t) i = Some (mk_mconfig q' t').
+Proof.
+  intros H.
+  assert ((s_sim ((PAIR (ext step_fun)) (enc q)) (enc t)) >* uiter (ext step_fun) (lam (O (enc q) (enc t)))).
+  { clear H.
+    unfold s_sim. now Lsimpl. }
+  rewrite H0 in H. clear H0.
+  change (lam (O (enc q) (enc t))) with (enc (mk_mconfig q t)) in H. 
+  eapply uiter_complete in H as (n & tps' & H). clear t'. rename tps' into t'.
+  revert H. generalize (mk_mconfig q t) as cfg. intros cfg H.
+  induction n in cfg, H |- *; cbn in *.
+  - congruence.
+  - destruct step_fun eqn:E.
+    + eapply IHn in H as (q'' & t'' & i & IH). exists q'', t'', (S i). rewrite <- IH.
+      cbn.
+      unfold step_fun in E.
+      destruct haltConf; try congruence; try inv E.
+      reflexivity.
+    + inv H. unfold step_fun in E.
+      destruct haltConf eqn:EE; try congruence. inv E.
+      exists (cstate cfg), (ctapes cfg), 1. cbn. rewrite EE. now destruct cfg.
+Qed.
+
+End TimeInvarianceNF.
+
+Definition sizeTM Σ n (M : TM Σ n) := (haltTime M + n* 130+ transTime M + 185 + 11).
+
+Theorem TimeInvarianceThesis_wrt_Termination_TM_to_L :
+  forall n Σ, exists encTM : TM Σ n -> term, exists encTps : tapes Σ n -> term,
+      exists sₛᵢₘ : term, exists C1 C2 : nat, forall M : TM Σ n, forall tps,
+              (forall tps' i q',
+                  loopM (mk_mconfig (start M) tps) i = Some (mk_mconfig q' tps') ->
+                  evalLe (C1 * (S i) * sizeTM M + C2) (sₛᵢₘ (encTM M) (encTps tps)) (encTps tps')) /\
+              (forall v, eval (sₛᵢₘ (encTM M) (encTps tps)) v -> exists q' tps' i, loopM (mk_mconfig (start M) tps) i = Some (mk_mconfig q' tps')).
+Proof.
+  intros n Σ.
+  pose (reg_sig := @registered_finType Σ).
+  unshelve eexists.
+  { intros M.
+    pose (reg_state := @registered_finType (state M)).
+    unshelve refine (PAIR (extT (step_fun (M := M))) (enc (start M))). 3:eapply step_fun_comp.
+  }
+  unshelve eexists.
+  { intros tps. exact (enc tps). }
+  exists s_sim.
+  eexists 10, 10.
+  intros M tps. split.
+  - intros tps' i q' H.
+    rewrite loopSumM_loopM_iff in H.
+    eapply TimeInvarianceNF_forward in H.
+    eapply evalLe_trans_rev with (k := 9) in H as (H1 & H & _).
+    2:{ unfold s_sim. Lsimpl. } cbn -[mult].
+    unfold s_sim. 
+    eapply evalIn_mono. Lsimpl. eapply (evalIn_refl 0). Lproc.
+    fold (sizeTM M). ring_simplify. lia.
+  - intros v (q' & t' & [] & H) % TimeInvarianceNF_backward. inv H. setoid_rewrite loopSumM_loopM_iff. eauto.
+Qed.
